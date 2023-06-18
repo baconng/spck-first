50fb7d491473caf8fed3678caa175147abfbc381
baconng <128912726+baconng@users.noreply.github.com>
2023-06-18 16:51:37 +0700

    spck.heades

---
+++ main.js
@@ -0,0 +1,16 @@
+let nav = document.getElementById("nav");
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 100){
+        nav.style.backgroundColor = "WHITE";
+        nav.style.color = 'white'
+        console.log("hd")
+        
+
+    }
+    else{
+        nav.style.backgroundColor = "transparent";
+
+    }
+    // console.log(window.scrollY);
+})
