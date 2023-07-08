cf883a0264a9ffa073c8acc0d4d824bf21125a41
baconng <128912726+baconng@users.noreply.github.com>
2023-07-08 18:39:01 +0700

    page
    add more page and fit footer

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ window.addEventListener('scroll', function() {
     if(window.scrollY > 100){
         nav.style.backgroundColor = "WHITE";
         nav.style.color = 'white'
-        console.log("hd")
-        
-
+        console.log("scroll")
     }
     else{
         nav.style.backgroundColor = "transparent";
